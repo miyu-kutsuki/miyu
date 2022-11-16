58ad3b49e574de31f88147e8b9dd3baaff0c1788
miyu <miyu.kutsuki@gmail.com>
2022-11-16 15:32:06 +0900

    新規会員登録画面(html/css)実装完了

--- safe/src/main/java/jp/co/kutsuki/safe/page/controller/UserRegistrationPageAction.java
+++ safe/src/main/java/jp/co/kutsuki/safe/page/controller/UserRegistrationPageAction.java
@@ -1,13 +1,9 @@
 package jp.co.kutsuki.safe.page.controller;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.kutsuki.safe.entity.User;
 
@@ -20,24 +16,14 @@ import jp.co.kutsuki.safe.entity.User;
 @RequestMapping("UserRegistration")
 public class UserRegistrationPageAction {
 	
-	@Autowired
-	HttpSession session;
-	
 	@ModelAttribute
 	public User setUpUser() {
 		return new User();
 	}
 	
 	@GetMapping
-	public String pageView(RedirectAttributes redirectAttributes) {
+	public String pageView() {
 		
-		//セッション有効チェック
-		boolean check = (boolean)session.getAttribute("check");
-		if(check) {
-			redirectAttributes.addFlashAttribute("msg", "セッションが無効です。");
-			return "redirect:Login";
-		}
-
 		return "userRegistration";
 	}
 }
