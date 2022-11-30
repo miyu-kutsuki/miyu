b06ee13686cd27544343cee16d8c0c16b97d1ddc
miyu <miyu.kutsuki@gmail.com>
2022-11-30 12:46:19 +0900

    行方不明者目撃情報登録の確認画面を追加実装

--- safe/src/main/java/jp/co/kutsuki/safe/controller/MissingPersonsSightingsAction.java
+++ safe/src/main/java/jp/co/kutsuki/safe/controller/MissingPersonsSightingsAction.java
@@ -1,23 +1,15 @@
 package jp.co.kutsuki.safe.controller;
 
-import java.time.LocalDate;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.kutsuki.safe.entity.MissingPersonsSightings;
-import jp.co.kutsuki.safe.entity.User;
 import jp.co.kutsuki.safe.safedb.repository.MissingPersonsSightingsRepository;
 
 /**
@@ -35,11 +27,7 @@ public class MissingPersonsSightingsAction {
 	HttpSession session;
 	
 	@RequestMapping(value="/MissingPersonsSightingsRegistration", method = RequestMethod.POST)
-	public String MissingPersonsSightingsView(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)@RequestParam(name = "date", required = false) LocalDate date, 
-			@RequestParam String gender, @RequestParam(name = "age", required = false) Integer age,
-			@RequestParam String detail, @RequestParam String prefectures, @RequestParam String municipalities, @RequestParam String other,
-			@Validated @ModelAttribute MissingPersonsSightings missingPersonsSighting, BindingResult bindingResult,
-			RedirectAttributes redirectAttributes, Model model) {
+	public String MissingPersonsSightingsView(RedirectAttributes redirectAttributes, Model model) {
 		
 		//セッション有効チェック
 		boolean check = (boolean)session.getAttribute("check");
@@ -48,25 +36,9 @@ public class MissingPersonsSightingsAction {
 			return "redirect:Login";
 		}
 		
-		//ログイン中のuser_idを取得
-		User userInformation = (User) session.getAttribute("user");
-		
-		//バリデーションの入力チェック
-		if(bindingResult.hasErrors()) {
-			redirectAttributes.addFlashAttribute("missingPersonsSightings", bindingResult);
-			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "missingPersonsSightings", bindingResult);
-		}else {
-			MissingPersonsSightings missingPersonsSightings = new MissingPersonsSightings();
-			missingPersonsSightings.setDate(date);
-			missingPersonsSightings.setGender(gender);
-			missingPersonsSightings.setAge(age);
-			missingPersonsSightings.setDetail(detail);
-			missingPersonsSightings.setPrefectures(prefectures);
-			missingPersonsSightings.setMunicipalities(municipalities);
-			missingPersonsSightings.setOther(other);
-			missingPersonsSightings.setUser_id(userInformation.getUser_id());
-			missingPersonsSightingsRepository.setMissingPersonsSightingsTable(missingPersonsSightings);
-		}
+		//missing_persons_sightingsテーブルに登録
+		MissingPersonsSightings missingPersonsSightings = (MissingPersonsSightings)session.getAttribute("missingPersonsSightings");
+		missingPersonsSightingsRepository.setMissingPersonsSightingsTable(missingPersonsSightings);
 		
 		return "redirect:MissingPersonsSightings";
 	}
