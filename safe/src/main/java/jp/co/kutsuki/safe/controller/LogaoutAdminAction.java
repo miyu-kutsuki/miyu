464f7eddd11288662a2168862e5362eb40ba1b11
miyu <miyu.kutsuki@gmail.com>
2022-12-07 17:43:31 +0900

    管理者画面のログアウト機能実装完了

---
+++ safe/src/main/java/jp/co/kutsuki/safe/controller/LogaoutAdminAction.java
@@ -0,0 +1,26 @@
+package jp.co.kutsuki.safe.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 管理者用ログアウト用コントローラー
+ * @author kutsuki
+ *
+ */
+@Controller
+public class LogaoutAdminAction {
+	
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(value="/LogoutAdminAction", method = RequestMethod.GET)
+	public String Logout() {
+		session.invalidate();
+		return "redirect:LoginAdmin";
+	}
+}
