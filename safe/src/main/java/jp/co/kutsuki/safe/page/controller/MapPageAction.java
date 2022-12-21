d9ff2874912fbe9a6f53cd12160e4c522efa052d
miyu <miyu.kutsuki@gmail.com>
2022-12-21 19:07:47 +0900

    javascriptで地図の表示まで実装完了

---
+++ safe/src/main/java/jp/co/kutsuki/safe/page/controller/MapPageAction.java
@@ -0,0 +1,21 @@
+package jp.co.kutsuki.safe.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 地図検索ページ遷移用コントローラー
+ * @author kutsuki
+ *
+ */
+@Controller
+@RequestMapping("MapSearch")
+public class MapPageAction {
+	
+	@GetMapping
+	public String loginPageView() {
+		
+		return "map";
+	}
+}
